76d37f67ac2e95f276f7840b06bccfbe7ee7cbd0
Nikolay Korotkov <korotkov.nf@gmail.com>
2020-01-31 21:55:48 +0300

    remove package dist

--- build/package/dist/decorators/InterfaceType.d.ts
+++
@@ -1,5 +0,0 @@
-import { DescriptionOptions, AbstractClassOptions, ResolveTypeOptions } from "./types";
-export declare type InterfaceTypeOptions = DescriptionOptions & AbstractClassOptions & ResolveTypeOptions;
-export declare function InterfaceType(): ClassDecorator;
-export declare function InterfaceType(options: InterfaceTypeOptions): ClassDecorator;
-export declare function InterfaceType(name: string, options?: InterfaceTypeOptions): ClassDecorator;
